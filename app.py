7ebf41e5cb90262572ac521f68009df202c20bdf
Exelion <autodiagbin@gmail.com>
2022-05-18 14:41:35 +0200

    Reformatte pp8

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import dearpygui.dearpygui as dpg
 import sorter
 
-
 min_h = 610
 min_w = 500
-
 
 dpg.create_context()
 
@@ -13,42 +11,50 @@ width, height, channels, data = dpg.load_image("data/logo.png")
 with dpg.texture_registry(show=False):
     dpg.add_static_texture(width, height, data, tag="logo")
 
+
 def proceed():
     dpg.hide_item("feedback")
     dpg.configure_item("modal_id", show=False)
     if dpg.get_value("path").strip():
-        sorting = sorter.Sorter(dir=dpg.get_value("path"))
+        sorting = sorter.Sorter(direct=dpg.get_value("path"))
         if sorting.check_dir():
             sorting.sort()
             print("Sorted")
         else:
-            dpg.set_value(item="feedback",value="Le chemin est invalide")
+            dpg.set_value(item="feedback", value="Le chemin est invalide")
             dpg.show_item("feedback")
     else:
-        dpg.set_value(item="feedback",value="Le chemin n'a pas été communiqué")
+        dpg.set_value(item="feedback", value="Le chemin n'a pas été communiqué")
         dpg.show_item("feedback")
 
-def set_directory(sender,app_data,user_data):
-    dpg.set_value("path",value=app_data["file_path_name"])
+
+def set_directory(sender, app_data, user_data):
+    dpg.set_value("path", value=app_data["file_path_name"])
+
 
 def load_shortcuts():
-    dpg.add_group(tag="shortcuts",horizontal=True)
+    dpg.add_group(tag="shortcuts", horizontal=True)
     for shortcut in sorter.shortcuts:
-        dpg.add_button(label=shortcut.name,callback=set_path,parent="shortcuts",tag=f"shortcut{shortcut.name}",user_data=shortcut)
-    
-def set_path(sender,user_data,app_data):
-    dpg.set_value("path",value=app_data)
+        dpg.add_button(label=shortcut.name, callback=set_path, parent="shortcuts", tag=f"shortcut{shortcut.name}",
+                       user_data=shortcut)
 
-with dpg.window(label="Trieur de dossier", width=min_h, height=min_w,min_size=(min_w,min_h),no_move=True,no_collapse=True):
+
+def set_path(sender, user_data, app_data):
+    dpg.set_value("path", value=app_data)
+
+
+with dpg.window(label="Trieur de dossier", width=min_h, height=min_w, min_size=(min_w, min_h), no_move=True,
+                no_collapse=True):
     dpg.add_group(tag="line_widget", horizontal=True)
-    dpg.add_image("logo",parent = "line_widget")
+    dpg.add_image("logo", parent="line_widget")
     dpg.add_group(tag="ui_folder", parent="line_widget")
 
-    dpg.add_file_dialog(directory_selector=True, show=False, callback=set_directory, tag="file_dialog_id",height=min_h-35,width=min_w-15,modal=True)
-    dpg.add_button(label="Choisir un dossier",callback=lambda: dpg.show_item("file_dialog_id"),parent="ui_folder")
-    dpg.add_input_text(label="Dossier", tag="path",enabled=False,parent = "ui_folder",width=350)
-    dpg.add_button(label="Trier le dossier", callback=proceed,tag="run")
-    dpg.add_text("The action has failed",color=(255,0,0),tag="feedback",show=False)
+    dpg.add_file_dialog(directory_selector=True, show=False, callback=set_directory, tag="file_dialog_id",
+                        height=min_h - 35, width=min_w - 15, modal=True)
+    dpg.add_button(label="Choisir un dossier", callback=lambda: dpg.show_item("file_dialog_id"), parent="ui_folder")
+    dpg.add_input_text(label="Dossier", tag="path", enabled=False, parent="ui_folder", width=350)
+    dpg.add_button(label="Trier le dossier", callback=proceed, tag="run")
+    dpg.add_text("The action has failed", color=(255, 0, 0), tag="feedback", show=False)
 
     with dpg.popup("run", mousebutton=dpg.mvMouseButton_Left, modal=True, tag="modal_id"):
         dpg.add_text("Êtes-vous sûr de vouloir trier ce dossier par catégories ?")
@@ -57,7 +63,7 @@ with dpg.window(label="Trieur de dossier", width=min_h, height=min_w,min_size=(m
     dpg.add_separator()
     load_shortcuts()
 
-dpg.create_viewport(title='Autodiag Tools', width=min_w, height=min_h,resizable=False)
+dpg.create_viewport(title='Autodiag Tools', width=min_w, height=min_h, resizable=False)
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.start_dearpygui()
--- sorter.py
+++ sorter.py
@@ -8,45 +8,47 @@ DOWNLOADS = HOME / "Downloads"
 MYDOCS = HOME / "Documents"
 DESKTOP = HOME / "Desktop"
 
-shortcuts = [DOWNLOADS,MYDOCS,DESKTOP]
+shortcuts = [DOWNLOADS, MYDOCS, DESKTOP]
 
 
-class Sorter():
-    def __init__(self,dir) -> None:
-        self.dir = Path(dir)
+def add_to_resolve(ext):
+    with open("resolve_list.json", "r") as f:
+        entries = json.load(f)
+    if ext not in entries:
+        entries.append(ext)
+        with open("resolve_list.json", "w") as f:
+            json.dump(entries, f, indent=4)
+
+
+def get_manifest_ext():
+    with open("sorted_files.json", "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+class Sorter:
+    def __init__(self, direct) -> None:
+        self.dir = Path(direct)
 
     def check_dir(self):
         if not self.dir.is_dir():
             return False
         return True
 
-    def get_manifestExt(self):
-        with open("sorted_files.json","r",encoding="utf-8") as f:
-            return json.load(f)
-        
     def get_files(self):
         return [f for f in self.dir.glob("*")]
-    
-    def add_to_resolve(self,ext):
-        with open("resolve_list.json","r") as f:
-            entries = json.load(f)
-        if ext not in entries:
-            entries.append(ext)
-            with open("resolve_list.json","w") as f:
-                json.dump(entries,f,indent=4)
-        
+
     def sort(self):
 
         files = self.get_files()
-        manifest = self.get_manifestExt()
+        manifest = get_manifest_ext()
 
         for f in files:
             found = False
             for ftype in manifest:
-                if found == True:
+                if found:
                     break
 
-                if (f.suffix).lower() in manifest[ftype]:
+                if f.suffix.lower() in manifest[ftype]:
                     Path.mkdir(self.dir / ftype, exist_ok=True)
                     if Path(self.dir / ftype / f.name).is_file():
                         f.rename(Path(f"{self.dir / ftype / f.name}_duplicate"))
@@ -56,20 +58,20 @@ class Sorter():
 
                 elif f.is_dir() and f.name not in manifest.keys():
                     Path.mkdir(self.dir / "Dossier", exist_ok=True)
-                    move(src=(self.dir / f),dst=(self.dir / "Dossier"))
+                    move(src=(self.dir / f), dst=(self.dir / "Dossier"))
                     found = True
 
                 elif f.name in manifest.keys():
                     found = True
                     break
-                
-            if found == False:
+
+            if not found:
                 Path.mkdir(self.dir / "Autre", exist_ok=True)
-                self.add_to_resolve(ext=f.suffix)
+                add_to_resolve(ext=f.suffix)
                 f.rename(self.dir / "Autre" / f.name)
-                
+
+
 if __name__ == "__main__":
     # sorter = Sorter(r"C:\Users\RMansouri\Downloads")
     # sorter.sort()
     pass
-
